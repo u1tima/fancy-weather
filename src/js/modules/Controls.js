7f0ff1054995d9b4c322f1a90c58bf761648a507
Vitali Semashkevich <vit-sema@yandex.ru>
2020-01-10 15:35:24 +0300

    feat: add Controls class

---
+++ src/js/modules/Controls.js
@@ -0,0 +1,19 @@
+export default class Controls {
+
+  constructor() {
+    this.btnChangeBackgroundImage = document.querySelector('.btn-refresh');
+
+    this.formSearch = document.querySelector('.form-search');
+    this.btnSearch = document.querySelector('.btn-search');
+    this.inputSearch = document.querySelector('.location-search');
+
+    this.temperatures = document.querySelectorAll('.temp');
+    this.btnCelsius = document.querySelector('.btn-celsius');
+    this.btnFarenheit = document.querySelector('.btn-farenheit');
+
+    this.buttonsLang = document.querySelectorAll('.btn-lang');
+    this.btnLangEn = document.querySelector('.btn-lang-en');
+    this.btnLangRu = document.querySelector('.btn-lang-ru');
+    this.btnLangBe = document.querySelector('.btn-lang-be');
+  }
+}
